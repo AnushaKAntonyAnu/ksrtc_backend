f0f13d66f07a0ef0b810e887f1ec88cfd07cb0a6
AnushaKAntony <anushakantony@gmail.com>
2024-06-14 00:33:07 +0530

    project completed

--- app.js
+++ app.js
@@ -101,6 +101,20 @@ app.post("/login",(req,res)=>{
         )
      })
 
+     app.post("/delete",(req,res)=>{
+        let input=req.body
+        busmodel.findByIdAndDelete(input._id).
+        then(
+            (response)=>{
+                res.json({"status":"success"})
+            }
+        ).catch(
+            (error)=>{
+                res.json({"status":"error"})
+            }
+        )
+    })
+
 app.listen(8080,()=>{
     console.log("server started")
 })
